9894fd15ca683c9e20c7aa1465ace07bacb89bf7
Name <name@gmail.com>
2019-05-30 10:55:57 +0400

    add autorization

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('login', views.login_view, name='login'),
+    path('logout', views.logout_view, name='logout'),
+    path('goto_login', views.goto_login, name='goto_login'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,12 @@
+from django.core.checks import messages
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login, logout
+
 
 # Create your views here.
 
@@ -12,9 +15,11 @@ def post_list(request):
     posts = Post.objects.order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
 
 def post_new(request):
     if request.method == "POST":
@@ -29,6 +34,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
+
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -42,3 +48,30 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def login_view(request):
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(username=email, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                # Redirect to a success page.
+                posts = Post.objects.order_by('published_date')
+                return render(request, 'blog/post_list.html', {'posts': posts})
+            else:
+                messages.error(request, 'username or password not correct')
+                #return render(request, 'blog/login.html')
+                # Return a 'disabled account' error message
+        else:
+            return render(request, 'blog/login.html')
+
+
+def logout_view(request):
+    logout(request)
+    #return render(request, 'blog/login.html')
+    return render(request, 'blog/login.html')
+
+def goto_login(request):
+    return render(request, 'blog/login.html')
